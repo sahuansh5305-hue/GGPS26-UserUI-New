e0ff4e9ebf087931fde262eb25341bb87b6e9abc
Ansh Sahu <sahuansh5305@gamil.com>
2025-12-31 21:04:42 +0530

    changed url

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 // ✅ ADD THIS AT THE VERY TOP
 // Redirect www to non-www
+// if (window.location.hostname === 'www.dewasps26.gautamsetu.com') {
+//   window.location.replace('https://dewasps26.gautamsetu.com');
+// }
+
 if (window.location.hostname === 'www.dewasps26.gautamsetu.com') {
-  window.location.replace('https://dewasps26.gautamsetu.com');
+  window.location.replace(
+    'https://www.dewasps26.gautamsetu.com' +
+    window.location.pathname +
+    window.location.search
+  );
 }
 
 import { createRoot } from "react-dom/client";
